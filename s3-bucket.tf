5722e546eb1ee213d818614d23a812a60bd8fb27
JuwonBae <jwbae@expernet.co.kr>
2023-03-17 13:10:35 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,12 @@
 module "s3-bucket" {
   source  = "app.terraform.io/JUWON-training/s3-bucket/aws"
   version = "2.8.0"
+  bucket_prefix = "jwbae"
 }
 
+/*
 module "bucket_prefix" {
     Description = "(Optional, Forces new resource) The name of the bucket. If omitted, Terraform will assign a random, unique name."
     Default = "jwbae"
 }
+*/
